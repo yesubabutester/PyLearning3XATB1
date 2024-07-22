51e74947d1b0e910c158a2c1711b509a75143358
yesubabutester <yesu.viswanadh123@gmail.com>
2024-07-22 18:41:00 +0530

    data types

--- Ex_03062024/Lab002.py
+++ Ex_03062024/Lab002.py
@@ -1,1 +1,16 @@
-print("testing lab")
+print("testing lab", end=' & ')
+print("HELLO WORLD")
+print("hello world","india", "123",  sep=' /')
+print(max(20, 50, 70, 80,90))
+
+x= 3.14
+print(type(x))
+
+a=123
+print(type(a))
+
+a="yesu"
+print(type(a))
+
+a=True
+print(type(a))
